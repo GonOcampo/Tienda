18d159cca1ea67bb4320092c66067661c12f52a7
Gonzalo <gonzalomatiasocampo@gmail.com>
2021-06-02 21:37:17 -0300

    Entregable 3 Components

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './NavBar.css';
+
+function NavBar () {
+    return(
+       
+        <div className="NavBar">
+          
+                <a href="NavBar">INICIO</a>
+                <a href="NavBar">PRODUCTOS</a>
+                <a href="NavBar">CLIENTES</a>
+                <a href="NavBar">DISEÑO</a>
+                <a href="NavBar">CONTACTO</a>
+        </div>
+    );
+}
+
+export default NavBar;
