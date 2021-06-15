ad9cb28abf3d8ca4434e1ea66a8f18df19b8425a
Gonzalo <gonzalomatiasocampo@gmail.com>
2021-06-15 19:28:06 -0300

    Item Count

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './ItemCount.css';
+
+
+const ItemCount = ({initial, stock, OnAdd}) => {
+
+  const [ Items, setItems] = useState(initial)
+  
+  const AddItems = () =>{
+    Items < stock &&
+        setItems(Items + 1)}
+  
+  const LessItems = () =>{
+          
+          Items > 1 &&
+              setItems(Items - 1)}
+
+    console.log(Items)
+
+    return(
+      <div>
+        <button onClick={LessItems} >-</button>
+        <span> {Items} </span>
+        <button onClick={AddItems} >+</button><br/>
+        <button onClick={OnAdd} >Agregar al Carrito</button>
+
+      </div>
+    )
+
+}
+
+export default ItemCount;
